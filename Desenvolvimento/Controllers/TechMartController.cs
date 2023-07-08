3a6dd3f9d0fbe01c6358bed7e8fceb5595d19c4c
MariaCeciliaa <mariacecilia@edu.unifil.br>
2023-07-08 15:04:11 -0300

    Codigo base do techmart

---
+++ Desenvolvimento/Controllers/TechMartController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechMart.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class TechMartController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product { Name = "Smartphone", Price = 500, Condition = "Novo" },
+                new Product { Name = "Tablet", Price = 300, Condition = "Usado" }
+            };
+
+            return products;
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Condition { get; set; }
+    }
+}
